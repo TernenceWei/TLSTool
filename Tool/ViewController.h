879259269e9d102503f4b195b8e66c55d535709f
TernenceWei <ternence@leomaster.com>
2015-12-28 14:07:29 +0800

    Initial Commit

---
+++ Tool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tool
+//
+//  Created by Ternence on 15/12/28.
+//  Copyright © 2015年 Leomaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tool
+//
+//  Created by Ternence on 15/12/28.
+//  Copyright © 2015年 Leomaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
